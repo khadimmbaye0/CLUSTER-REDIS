dc44141a5dc82b661b4baba483a3660fdafbe2ef
khadimmbaye0 <khadimmbaye4002@gmail.com>
2026-04-26 15:47:43 +0000

    correctifs

--- script/redis-master-1.sh
+++ script/redis-master-1.sh
@@ -7,7 +7,7 @@ redis-server /etc/redis.conf &
 sleep 5
 
 # Creation du cluster redis
-redis-cli --cluster create redis-master-1:7001 redis-master-2:7002 redredis-cli --cluster create redis-master-1:7001 redis-master-2:7002 redis-master-3:7003 redis-slave-1:7004 redis-slave-2:7005 redis-slave-3:7006 redis-slave-4:7007 redis-slave-5:7008 redis-slave-6:7009 --cluster-replicas 2 --cluster-yes
+redis-cli --cluster create redis-master-1:7001 redis-master-2:7002 redis-master-3:7003 redis-slave-1:7004 redis-slave-2:7005 redis-slave-3:7006 redis-slave-4:7007 redis-slave-5:7008 redis-slave-6:7009 --cluster-replicas 2 --cluster-yes
 is-master-3:7003 redis-slave-1:7004 redis-slave-2:7005 redis-slave-3:7006 redis-slave-4:7007 redis-slave-5:7008 redis-slave-6:7009 --cluster-replicas 2 --cluster-yes
 
 # Log
