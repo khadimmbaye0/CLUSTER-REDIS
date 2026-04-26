b848364d73b94a0acbe53e4309a061ff501eea5b
khadimmbaye0 <khadimmbaye4002@gmail.com>
2026-04-26 15:20:21 +0000

    script de creation

---
+++ script/redis-master-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Demarrage du premier serveur redis avec les configuration specifiees dans le fichier
+redis-server /etc/redis.conf &
+
+# Attendre un moment, pour permettre au serveur de demarrer
+sleep 5
+
+# Creation du cluster redis
+redis-cli --cluster create redis-master-1:7001 redis-master-2:7002 redis-master-3:7003 redis-slave-1:7004 redis-slave-2:7005 redis-slave-3:7006 redis-slave-4:7007 redis-slave-5:7008 redis-slave-6:7009 --cluster-replicas 2 --cluster-yes
+
+# Log
+echo "Cluster redis deploye avec succes"
+
+# Garder le script en execution en background
+touch /dev/null
+tail -f /dev/null
